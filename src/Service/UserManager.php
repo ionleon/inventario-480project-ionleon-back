aa9ad741963deba28c3f302095e064220e6d4046
ionleon <ionleon@cuatroochenta.com>
2026-04-20 12:10:11 +0200

    -Creation of UserManager

---
+++ src/Service/UserManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\AppUser;
+
+class UserManager
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    )
+    {}
+
+    public function create(AppUser $user, string $plainPassword): AppUser
+    {
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $user->setFirstTime(true);
+        $user->setIsActive(true);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+}
