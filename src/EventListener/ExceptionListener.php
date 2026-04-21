3daf50495b8a408218796018838cdb2897e1fe98
ionleon <ionleon@cuatroochenta.com>
2026-04-21 16:39:15 +0200

    - Method onKernelException fully created - Added route to services.yaml

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,7 +2,32 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
 class ExceptionListener
 {
+        public function onKernerlException(ExceptionEvent $event): void
+        {
+
+            $exception = $event->getThrowable();
+
+            $responseData = [
+                'error' => 'Internal Server Error',
+                'message' => $exception->getMessage(),
+                'code' => 500
+            ];
+
+            if($exception instanceof HttpExceptionInterface) {
+                $responseData['code'] = $exception->getStatusCode();
+                $responseData['error'] = 'HTTP Error';
+            }
+
+            $response = new JsonResponse($responseData, $responseData['code']);
+
+            $event->setResponse($response);
+
+        }
 
 }
