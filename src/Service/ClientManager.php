343032edf62ee6d53165cbf1ce8afc11eb606a0f
ionleon <ionleon@cuatroochenta.com>
2026-04-23 16:27:52 +0200

    - Created business logic in ClientManager.php

--- src/Service/ClientManager.php
+++ src/Service/ClientManager.php
@@ -2,7 +2,53 @@
 
 namespace App\Service;
 
+use App\Entity\Client;
+use App\Repository\SectorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class ClientManager
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private SectorRepository $sectorRepository
+    ) {}
 
+    public function createClient(array $data): Client
+    {
+        $client = new Client();
+        return $this->updateClient($client, $data);
+    }
+
+    public function updateClient(Client $client, array $data): Client
+    {
+        if (isset($data['name'])) {
+            $client->setName($data['name'] ?? $client->getName());
+        }
+
+        if (isset($data['isActive'])) {
+            $client->setIsActive($data['isActive'] ?? $client->isActive() ?? true);
+        }
+
+        if (isset($data['sector_id'])) {
+            $sector = $this->sectorRepository->find($data['sector_id']);
+            if (!$sector) {
+                throw new NotFoundHttpException('Sector not found');
+            }
+            $client->setSector($sector);
+        } elseif (!$client->getSector()) {
+            throw new \InvalidArgumentException('sector_id is required');
+        }
+
+        $this->em->persist($client);
+        $this->em->flush();
+
+        return $client;
+    }
+
+    public function deleteClient(Client $client): void
+    {
+        $this->em->remove($client);
+        $this->em->flush();
+    }
 }
