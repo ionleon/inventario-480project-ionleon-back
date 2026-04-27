8c345d741aca466870ca0805ba6f2b5e5dd69dbd
ionleon <ionleon@cuatroochenta.com>
2026-04-27 10:45:29 +0200

    - SectorFixtures.php load method and reference constant implemented

--- src/DataFixtures/SectorFixtures.php
+++ src/DataFixtures/SectorFixtures.php
@@ -2,7 +2,24 @@
 
 namespace App\DataFixtures;
 
-class SectorFixtures
-{
+use App\Entity\Sector;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
+class SectorFixtures extends Fixture
+{
+    public const SECTOR_REF = 'sector-';
+
+    public function load(ObjectManager $manager): void
+    {
+        $sectores = ['Tecnología', 'Salud', 'Finanzas', 'Educación'];
+
+        foreach ($sectores as $key => $nombre) {
+            $sector = new Sector();
+            $sector->setName($nombre);
+            $manager->persist($sector);
+            $this->addReference(self::SECTOR_REF . $key, $sector);
+        }
+        $manager->flush();
+    }
 }
