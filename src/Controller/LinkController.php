7687f6f67bf5121343d46478a150d61d91793e7f
ionleon <ionleon@cuatroochenta.com>
2026-04-27 09:30:17 +0200

    - CRUD methods implemented in controller

--- src/Controller/LinkController.php
+++ src/Controller/LinkController.php
@@ -2,17 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Link;
+use App\Repository\LinkRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+#[Route('/link', name: 'app_link')]
 final class LinkController extends AbstractController
 {
-    #[Route('/link', name: 'app_link')]
-    public function index(): Response
+    public function __construct(
+        private LinkManager $manager,
+        private LinkRepository $repository
+    ) {}
+
+    #[Route('', name: 'link_index', methods: ['GET'])]
+    public function index(): JsonResponse
     {
-        return $this->render('link/index.html.twig', [
-            'controller_name' => 'LinkController',
-        ]);
+        $links = $this->repository->findAll();
+        return $this->json($links, 200, [], ['groups' => 'link:read']);
     }
+
+    #[Route('/{id}', name: 'link_show', methods: ['GET'])]
+    public function show(Link $link): JsonResponse
+    {
+        return $this->json($link, 200, [], ['groups' => 'link:read']);
+    }
+
+    #[Route('',name: 'link_create', methods:['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $data =  json_decode($request->getContent(), true);
+        try {
+            $link = $this->manager->create($data);
+            return $this->json([], 201, [], []);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    #[Route('/{id}',name: 'link_update', methods:['PUT'])]
+    public function update(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        try {
+            $this->manager->update($data);
+            return $this->json([], 201, [], []);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    #[Route('/{id}',name: 'link_delete', methods:['DELETE'])]
+    public function delete(Link $link): JsonResponse
+    {
+        $this->manager->delete($link);
+        return $this->json(null, 204);
+    }
+
+
 }
