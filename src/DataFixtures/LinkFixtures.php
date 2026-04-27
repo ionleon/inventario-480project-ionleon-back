5b53528bc54b53376cf157786303832081748c2a
ionleon <ionleon@cuatroochenta.com>
2026-04-27 10:00:41 +0200

    - Implemented load and getDependencies methods

--- src/DataFixtures/LinkFixtures.php
+++ src/DataFixtures/LinkFixtures.php
@@ -2,7 +2,43 @@
 
 namespace App\DataFixtures;
 
-class LinkFixtures
+use App\Entity\Development;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class LinkFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+        $enviroments = Enviroment::cases();
+
+        // Creamos entre 1 y 3 links para cada Development creado anteriormente
+        for ($i = 0; $i < 10; $i++) {
+            $dev = $this->getReference(DevelopmentFixtures::DEVELOPMENT_REFERENCE . $i, Development::class);
+
+            $numLinks = rand(1, 3);
+            for ($j = 0; $j < $numLinks; $j++) {
+                $link = new Link();
+                $link->setUrl($faker->url());
+                $link->setEnviroment($faker->randomElement($enviroments));
+                $link->setDevelopment($dev);
+
+                $manager->persist($link);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            DevelopmentFixtures::class,
+        ];
+    }
+
 
 }
