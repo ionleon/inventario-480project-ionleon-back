b1af4aa5c8b863379d74d387139078e427c70c18
ionleon <ionleon@cuatroochenta.com>
2026-04-27 15:35:47 +0200

    - Basic business logic created for TechnologyManager.php

--- src/Service/TechnologyManager.php
+++ src/Service/TechnologyManager.php
@@ -2,7 +2,47 @@
 
 namespace App\Service;
 
+use App\Entity\Technology;
+use App\Repository\TechnologyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 class TechnologyManager
 {
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private TechnologyRepository $repository
+    ) {}
+
+    public function create(array $data): Technology {
+        //Añadir si pasamos ID por front
+        //if (!isset($data['id'], $data['name'])) {
+        if (!isset($data['name'])) {
+            throw new \InvalidArgumentException('Faltan campos obligatorios (name).');
+        }
+
+        $technology = new Technology();
+
+        return $this->save($technology, $data);
+
+    }
+
+    public function save(Technology $technology,array $data): Technology
+    {
+        $technology->setName($data['name'] ?? $technology->getName());
+        $this->em->remove($technology);
+        $this->em->flush();
+
+        return $technology;
+    }
+
+    public function delete(Technology $technology): void {
+        if (!$technology->getDevelopments()->isEmpty()) {
+            throw new \LogicException('Technology in use, cannot delete.');
+        }
+
+        $this->em->remove($technology);
+        $this->em->flush();
+    }
 
 }
