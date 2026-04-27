25162d4059e977b2e0b8ae665695083479167a12
ionleon <ionleon@cuatroochenta.com>
2026-04-27 11:45:37 +0200

    - ProjectFixtures.php now references ClientFixtures.php, along with a correct entity construction.

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -8,25 +8,26 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Uid\Uuid;
 
 
-class ProjectFixtures extends Fixture
+class ProjectFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public const PROJECT_REF = 'project-';
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
-        $client = new Client();
-        $client->setName($faker->company());
-        $manager->persist($client);
 
         for ($i = 0; $i < 5; $i++) {
             $project = new Project();
+            $project->setId(Uuid::v7());
             $project->setName($faker->sentence(3));
             $project->setDescription($faker->paragraph());
             $project->setStartedAt($faker->dateTimeBetween('-1 year', 'now'));
             $project->setIsActive($faker->boolean(80));
+
+            $client = $this->getReference(ClientFixtures::CLIENT_REF . rand(0, 4), Client::class);
             $project->setClient($client);
 
             $manager->persist($project);
@@ -35,5 +36,12 @@ class ProjectFixtures extends Fixture
 
         $manager->flush();
 
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ClientFixtures::class
+        ];
     }
 }
