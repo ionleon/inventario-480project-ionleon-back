7642c78f0db9d89e6d2b70e7bd8d6ea96d2f8b0a
ionleon <ionleon@cuatroochenta.com>
2026-04-27 12:06:02 +0200

    - ContactFixtures.php implemented logic.

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,7 +2,50 @@
 
 namespace App\DataFixtures;
 
-class ContactFixtures
-{
+use App\Entity\Client;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
+class ContactFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('es_ES');
+
+        // Iteramos sobre los 5 clientes que creamos en ClientFixtures
+        for ($i = 0; $i < 5; $i++) {
+            /** @var Client $client */
+            $client = $this->getReference(ClientFixtures::CLIENT_REF . $i, Client::class);
+
+            // Creamos entre 1 y 3 contactos por cliente
+            $numContacts = rand(1, 3);
+
+            for ($j = 0; $j < $numContacts; $j++) {
+                $contact = new Contact();
+                $contact->setFullName($faker->name());
+                $contact->setPhoneNumber($faker->phoneNumber());
+                $contact->setEmail($faker->safeEmail());
+                $contact->setIsActive(true);
+
+                // El primer contacto del bucle será el principal (isMain)
+                $contact->setIsMain($j === 0);
+
+                $contact->setNote($faker->boolean(50) ? $faker->sentence() : null);
+                $contact->setClient($client);
+
+                $manager->persist($contact);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ClientFixtures::class,
+        ];
+    }
 }
