a613b5d8789b791e1430367df1b859f3d2b9daa6
ionleon <ionleon@cuatroochenta.com>
2026-04-27 11:46:11 +0200

    - Fixed entity creation.

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -3,10 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\Client;
+use App\Entity\Sector;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Uid\Uuid;
 
 
 class ClientFixtures extends Fixture implements DependentFixtureInterface
@@ -20,8 +22,11 @@ class ClientFixtures extends Fixture implements DependentFixtureInterface
 
         for ($i = 0; $i < 5; $i++) {
             $client = new Client();
+            $client->setId(Uuid::v7());
             $client->setName($faker->company());
             $client->setIsActive(true);
+
+            $client->setSector($this->getReference(SectorFixtures::SECTOR_REF . rand(0, 3), Sector::class));
 
             $manager->persist($client);
             $this->addReference(self::CLIENT_REF . $i, $client);
