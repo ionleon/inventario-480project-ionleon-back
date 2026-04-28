d2eabcf3bd2b959e074ec0abee4e614c145d79bf
ionleon <ionleon@cuatroochenta.com>
2026-04-28 10:21:29 +0200

    - Unique labels added to attribute id.

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Serializer\Attribute\SerializedName;
 use Symfony\Component\Uid\Uuid;
@@ -14,6 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[UniqueEntity(fields: ['id'], message: 'This ID already in use.')]
 class Project
 {
     #[ORM\Id]
