c1e5624b9d5892449b1808cb4fc62eac5363cfc1
ionleon <ionleon@cuatroochenta.com>
2026-04-28 11:08:52 +0200

    - Created show and create methods.

--- src/Controller/TimeEntryController.php
+++ src/Controller/TimeEntryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\TimeEntry;
 use App\Repository\TimeEntryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -19,9 +22,28 @@ final class TimeEntryController extends AbstractController
     ) {}
 
     #[Route('', name: 'time_entry_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(): JsonResponse
     {
         $timeEntries = $this->repository->findAll();
         return $this->json($timeEntries, 200, [], ['groups' => ['time:read']]);
+    }
+
+    #[Route('/{id}', name: 'time_entry_show', methods: ['GET'])]
+    public function show(TimeEntry $timeEntry): JsonResponse
+    {
+        return $this->json($timeEntry, 200, [], ['groups' => ['time:read']]);
+    }
+
+    #[Route('', name: 'time_entry_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        try {
+            $timeEntry = $this->manager->create($data);
+            return $this->json([], 200, [], ['groups' => ['time:read']]);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 400);
+        }
+
     }
 }
