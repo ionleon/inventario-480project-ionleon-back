ad65cd84408fbb4285317e91b0adfbd0adc457d9
ionleon <ionleon@cuatroochenta.com>
2026-04-28 09:15:27 +0200

    - Index method created

--- src/Controller/SectorController.php
+++ src/Controller/SectorController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\SectorRepository;
+use App\Service\SectorManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/sectors', name: 'app_sector')]
 final class SectorController extends AbstractController
 {
-    #[Route('/sector', name: 'app_sector')]
+    public function __construct(
+        private SectorManager $manager,
+        private SectorRepository $repository
+    ) {}
+
+    #[Route('', name: 'sector_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('sector/index.html.twig', [
-            'controller_name' => 'SectorController',
-        ]);
+        $sectors = $this->repository->findAll();
+        return $this->json($sectors, 200, [], ['groups' => 'sector:read']);
     }
 }
