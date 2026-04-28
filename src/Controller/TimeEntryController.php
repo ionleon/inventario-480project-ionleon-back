0d89c91d71d5b5eda531cd11535a21f4ba71f76a
ionleon <ionleon@cuatroochenta.com>
2026-04-28 11:02:25 +0200

    - Created index method

--- src/Controller/TimeEntryController.php
+++ src/Controller/TimeEntryController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\TimeEntryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+
+#[Route('/time-entries', name: 'app_time_entry')]
+#[IsGranted('ROLE_USER')]
 final class TimeEntryController extends AbstractController
 {
-    #[Route('/time/entry', name: 'app_time_entry')]
+    public function __construct(
+        private TimeEntryManager $manager,
+        private TimeEntryRepository $repository
+    ) {}
+
+    #[Route('', name: 'time_entry_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('time_entry/index.html.twig', [
-            'controller_name' => 'TimeEntryController',
-        ]);
+        $timeEntries = $this->repository->findAll();
+        return $this->json($timeEntries, 200, [], ['groups' => ['time:read']]);
     }
 }
