c48a8773170d72e3b64e085f6b3fdfacdbc8b43c
ionleon <ionleon@cuatroochenta.com>
2026-04-28 10:25:12 +0200

    - Migration executed so changes are reflected on the database.

---
+++ migrations/Version20260428082420.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260428082420 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C0D6212A5E237E06 ON development (name)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_C0D6212A5E237E06');
+    }
+}
