98834bf260e3d8b299e3461cb1956a14b634aedd
ionleon <ionleon@cuatroochenta.com>
2026-04-28 10:36:56 +0200

    - Constructor with dependency injection created

--- src/Service/TimeEntryManager.php
+++ src/Service/TimeEntryManager.php
@@ -2,7 +2,15 @@
 
 namespace App\Service;
 
+use App\Entity\TimeEntry;
+use App\Repository\TimeEntryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 class TimeEntryManager
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private TimeEntryRepository $repository
+    ) {}
 
 }
