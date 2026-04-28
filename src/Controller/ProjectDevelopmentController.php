5f97344b8265961e9a7c1ab1d2f9eee420c5001f
ionleon <ionleon@cuatroochenta.com>
2026-04-28 16:29:02 +0200

    - Created ProjectDevelopmentController.php constructor.

--- src/Controller/ProjectDevelopmentController.php
+++ src/Controller/ProjectDevelopmentController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\DevelopmentRepository;
+use App\Service\DevelopmentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/projects/{id}/developments', name: 'app_project_users_development')]
 final class ProjectDevelopmentController extends AbstractController
 {
-    #[Route('/project/development', name: 'app_project_development')]
+    public function __construct(
+        private DevelopmentManager $devManager,
+        private DevelopmentRepository $repository
+    )
+    {}
+
+    #[Route('', name: 'project_users_development_index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('project_development/index.html.twig', [
