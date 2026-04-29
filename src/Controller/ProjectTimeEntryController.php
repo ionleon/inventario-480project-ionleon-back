b243558923263417538256bc6c49564918518343
ionleon <ionleon@cuatroochenta.com>
2026-04-29 13:32:54 +0200

    - Added update and create methods.

--- src/Controller/ProjectTimeEntryController.php
+++ src/Controller/ProjectTimeEntryController.php
@@ -2,26 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Entity\TimeEntry;
 use App\Repository\TimeEntryRepository;
 use App\Service\TimeEntryManager;
+use Exception;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/projects/{id}/time-entries', name: 'app_project_time_entry')]
+#[IsGranted(new Expression("is_granted('ROLE_ADMIN') or is_granted('ROLE_EMPLOYEE')"))]
 final class ProjectTimeEntryController extends AbstractController
 {
     public function __construct(
         private TimeEntryManager $teManager,
         private TimeEntryRepository $teRepository
-    )
-    {}
+    ) {}
 
     #[Route('', name: 'project_time_entry_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Project $project): JsonResponse
     {
-        return $this->render('project_time_entry/index.html.twig', [
-            'controller_name' => 'ProjectTimeEntryController',
-        ]);
+        $user = $this->isGranted('ROLE_ADMIN') ? null : $this->getUser();
+
+        $timeEntries = $this->teManager->getAllByProjects($project, $user);
+
+        return $this->json($timeEntries, 200, [], ['groups' => 'time:read']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('', name: 'project_time_entry_create', methods: ['POST'])]
+    public function create(Project $project, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $timeEntry = $this->teManager->create($data);
+        return $this->json([], 201, [], ['groups' => 'time:read']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('{timeEntryId}', name: 'project_time_entry_update', methods: ['PUT'])]
+    public function update(
+        Project $project,
+        #[MapEntity(mapping: ['id' => 'timeEntryId'])] TimeEntry $timeEntry,
+        Request $request
+    ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $timeEntry = $this->teManager->save($timeEntry, $data);
+        return $this->json([], 201, [], ['groups' => 'time:read']);
     }
 }
