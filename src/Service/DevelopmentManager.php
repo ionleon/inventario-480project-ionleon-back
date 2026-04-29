34db9a6c363dc98094504d3c5f3d3fb3518f4033
ionleon <ionleon@cuatroochenta.com>
2026-04-29 08:54:34 +0200

    - Refactored DevelopmentManager.php so compatible with ProjectDevelopmentController.php.

--- src/Service/DevelopmentManager.php
+++ src/Service/DevelopmentManager.php
@@ -6,6 +6,8 @@ use App\Entity\Development;
 use App\Entity\Project;
 use App\Entity\Technology;
 use App\Repository\DevelopmentRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\TechnologyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Uid\Uuid;
@@ -14,28 +16,20 @@ class DevelopmentManager
 {
     public function __construct(
         private EntityManagerInterface $em,
-        private DevelopmentRepository $repository
+        private DevelopmentRepository $devRepository,
+        private TechnologyRepository $technologyRepository,
+        private ProjectRepository $projectRepository,
     ) {}
 
-    public function create(array $data): Development
+    public function create(Project $project, array $data): Development
     {
-        if (!isset($data['id'], $data['projectId'], $data['technologyId'], $data['name'])) {
-            throw new \InvalidArgumentException('Faltan campos obligatorios (id, projectId, technologyId, name).');
+        if (!isset($data['id'], $data['technologyId'], $data['name'])) {
+            throw new \InvalidArgumentException('Faltan campos obligatorios (id, technologyId, name).');
         }
 
         $development = new Development();
         $development->setId(Uuid::fromString($data['id']));
-
-
-        $project = $this->em->getRepository(Project::class)->find($data['projectId']);
-        $technology = $this->em->getRepository(Technology::class)->find($data['technologyId']);
-
-        if (!$project || !$technology) {
-            throw new NotFoundHttpException('Proyecto o Tecnología no encontrados.');
-        }
-
         $development->setProject($project);
-        $development->setTechnology($technology);
 
         return $this->save($development, $data);
     }
@@ -46,15 +40,13 @@ class DevelopmentManager
         $development->setDescription($data['description'] ?? $development->getDescription());
         $development->setUrlRepository($data['urlRepository'] ?? $development->getUrlRepository());
 
-        // Si se intenta cambiar el proyecto o tecnología en un update
-        if (isset($data['projectId'])) {
-            $project = $this->em->getRepository(Project::class)->find($data['projectId']);
-            if ($project) $development->setProject($project);
-        }
 
         if (isset($data['technologyId'])) {
-            $technology = $this->em->getRepository(Technology::class)->find($data['technologyId']);
-            if ($technology) $development->setTechnology($technology);
+            $technology = $this->technologyRepository->find($data['technologyId']);
+            if (!$technology) {
+                throw new NotFoundHttpException('Proyecto o Tecnología no encontrados.');
+            }
+            $development->setTechnology($technology);
         }
 
         $this->em->persist($development);
