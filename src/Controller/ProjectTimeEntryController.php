a22ff7bf360afa1225852228293541e266440070
ionleon <ionleon@cuatroochenta.com>
2026-04-29 11:55:50 +0200

    - ProjectTimeEntryController.php constructor created.

--- src/Controller/ProjectTimeEntryController.php
+++ src/Controller/ProjectTimeEntryController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\TimeEntryRepository;
+use App\Service\TimeEntryManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/projects/{id}/time-entries', name: 'app_project_time_entry')]
 final class ProjectTimeEntryController extends AbstractController
 {
-    #[Route('/project/time/entry', name: 'app_project_time_entry')]
+    public function __construct(
+        private TimeEntryManager $teManager,
+        private TimeEntryRepository $teRepository
+    )
+    {}
+
+    #[Route('', name: 'project_time_entry_index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('project_time_entry/index.html.twig', [
