dc6f4b6b073be825de434ccef59606b2fddd4148
ionleon <ionleon@cuatroochenta.com>
2026-04-30 16:05:03 +0200

    - Implemented calls to manager with slight role check.

--- src/Controller/UserTimeEntryController.php
+++ src/Controller/UserTimeEntryController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\AppUser;
+use App\Entity\TimeEntry;
+use App\Repository\TimeEntryRepository;
+
+use App\Service\TimeEntryManager;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/users/{id}/time-entries', name: 'app_user_time_entry')]
 final class UserTimeEntryController extends AbstractController
 {
-    #[Route('/user/time/entry', name: 'app_user_time_entry')]
-    public function index(): Response
+
+    public function __construct(
+      private TimeEntryManager $teManager,
+      private TimeEntryRepository $teRepostory
+    ) {}
+
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(AppUser $user): Response
     {
-        return $this->render('user_time_entry/index.html.twig', [
-            'controller_name' => 'UserTimeEntryController',
-        ]);
+        if(!$this->isGranted('ROLE_ADMIN') && $user !== $this->getUser()){
+            throw $this->createAccessDeniedException('Cannot see hours of other users.');
+        }
+
+        $entries = $this->teRepostory->findByUser($user);
+        return $this->json($entries, 200, [], ['groups' => ['time:read']]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('', name: 'create', methods: ['POST'])]
+    public function create(AppUser $user, Request $request): Response
+    {
+        if(!$this->isGranted('ROLE_ADMIN') && $user !== $this->getUser()){
+            throw $this->createAccessDeniedException('Cannot see hours of other users.');
+        }
+        $data = json_decode($request->getContent(), true);
+
+        $timeEntry = $this->teManager->create(data: $data, targetUser:  $user);
+        return $this->json([], 201, [], ['groups' => ['time:read']]);
     }
 }
