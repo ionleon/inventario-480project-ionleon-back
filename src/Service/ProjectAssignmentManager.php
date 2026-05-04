16f6829831be83209fd73631948f0be149a7f1d3
ionleon <ionleon@cuatroochenta.com>
2026-05-04 11:31:46 +0200

    - Refactored remove and create methods, syncProjectUsers now uses a transaction.

--- src/Service/ProjectAssignmentManager.php
+++ src/Service/ProjectAssignmentManager.php
@@ -36,16 +36,11 @@ class ProjectAssignmentManager
             throw new Exception('User or Role not found', 404);
         }
 
-
-        $exists = $this->puRepository->findOneBy([
-           'project' => $project,
-           'appUser' => $userId
-        ]);
+        $exists = $this->puRepository->findOneByProjectAndUser($project, $user);
 
         if ($exists) {
-            throw new Exception('user is already assigned', 409);
+            throw new Exception('User is already assigned', 409);
         }
-
 
         $assignment = new ProjectUser();
         $assignment->setProject($project);
@@ -61,29 +56,40 @@ class ProjectAssignmentManager
 
     public function syncProjectUsers(Project $project, array $userData): void
     {
-        $currentAssignments = [];
-        foreach ($project->getProjectUsers() as $assignment) {
-            $currentAssignments[$assignment->getAppUser()->getId()->toRfc4122()] = $assignment;
-        }
+        $this->em->wrapInTransaction(function () use ($project, $userData) {
+            $currentAssignments = [];
+            foreach ($project->getProjectUsers() as $assignment) {
+                $currentAssignments[$assignment->getAppUser()->getId()->toRfc4122()] = $assignment;
+            }
 
-        foreach ($userData as $data) {
-            $userId = $data['user_id'];
-            $roleId = $data['role_id'];
+            foreach ($userData as $data) {
+                $userId = $data['user_id'] ?? null;
+                $roleId = $data['role_id'] ?? null;
 
-            if (isset($currentAssignments[$userId])) {
-                $role = $this->roleRepository->find($roleId);
-                $currentAssignments[$userId]->setProjectRole($role);
-                unset($currentAssignments[$userId]);
-            } else {
-                $newUser = $this->userRepository->find($userId);
-                $role = $this->roleRepository->find($roleId);
+                if (!$userId || !$roleId) continue;
 
-                $assignment = new ProjectUser();
-                $assignment->setProject($project);
-                $assignment->setAppUser($newUser);
-                $assignment->setProjectRole($role);
+                if (isset($currentAssignments[$userId])) {
+                    $role = $this->roleRepository->find($roleId);
+                    if ($role) {
+                        $currentAssignments[$userId]->setProjectRole($role);
+                    }
+                    unset($currentAssignments[$userId]);
+                } else {
+                    $newUser = $this->userRepository->find($userId);
+                    $role = $this->roleRepository->find($roleId);
 
-                $this->em->persist($assignment);
+                    if (!$role) {
+                        throw new Exception("Role with ID $roleId not found", 404);
+                    }
+
+                    if($newUser && $role) {
+                        $assignment = new ProjectUser();
+                        $assignment->setProject($project);
+                        $assignment->setAppUser($newUser);
+                        $assignment->setProjectRole($role);
+                        $this->em->persist($assignment);
+                    }
+                }
             }
 
             foreach ($currentAssignments as $oldAssignment) {
@@ -91,34 +97,27 @@ class ProjectAssignmentManager
             }
 
             $this->em->flush();
+        });
 
-        }
+
+
     }
 
     /**
      * @throws Exception
      */
-    public function removeAssignment(Project $project, string $userId): void
+    public function removeAssignment(Project $project, AppUser $user): void
     {
-        $user = $this->userRepository->find($userId);
 
-        if (!$user ) {
-            throw new Exception('User not found', 404);
+        $assignment = $this->puRepository->findOneByProjectAndUser($project, $user);
+
+        if (!$assignment) {
+            throw new Exception('Assignemt not found', 404);
         }
 
-        $assignment = $this->puRepository->findOneBy([
-           'project' => $project,
-           'appUser' => $user
-        ]);
-
-        if ($assignment) {
-            $this->em->remove($assignment);
-            $this->em->flush();
-        }
-
+        $this->em->remove($assignment);
+        $this->em->flush();
 
     }
-
-
 
 }
