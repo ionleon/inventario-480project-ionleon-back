239dbdd1435c2d53698ab0d250de308007a92d57
ionleon <ionleon@cuatroochenta.com>
2026-05-04 11:30:28 +0200

    - Created query builders to get all assignments of a  project and the assignment of a project and a user.

--- src/Repository/ProjectUserRepository.php
+++ src/Repository/ProjectUserRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repository;
 
+use App\Entity\AppUser;
+use App\Entity\Project;
 use App\Entity\ProjectUser;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,6 +16,30 @@ class ProjectUserRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ProjectUser::class);
+    }
+
+    public function findAllByProjects(Project $project): array
+    {
+        return $this->createQueryBuilder('pu')
+            ->innerJoin('pu.appUser', 'u')->addSelect('u')
+            ->innerJoin('pu.projectRole', 'r')->addSelect('r')
+            ->where('pu.project = :project')
+            ->setParameter('project', $project)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findOneByProjectAndUser(Project $project, AppUser $user): array
+    {
+        return $this->createQueryBuilder('pu')
+            ->innerJoin('pu.appUser', 'u')->addSelect('u')
+            ->innerJoin('pu.projectRole', 'r')->addSelect('r')
+            ->where('pu.project = :project')
+            ->andWhere('pu.appUser = :user')
+            ->setParameter('project', $project)
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     //    /**
