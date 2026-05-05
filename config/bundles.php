cb4dce15dd5c55a73da1c73fc22a6c8f29c065c3
ionleon <ionleon@cuatroochenta.com>
2026-05-05 11:49:06 +0200

    - Installed gesdinet/jwt-refresh-token-bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
