3c74988120867bd8e31ee01ad6e8fc419d34e856
ionleon <ionleon@cuatroochenta.com>
2026-05-05 12:42:03 +0200

    - Changed AuthService.php name to AuthManager.php.

---
+++ src/Service/AuthManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+
+class AuthManager
+{
+    public function __construct(
+        private readonly RefreshTokenManagerInterface $refreshTokenManager,
+        private readonly EntityManagerInterface $em
+    ) {}
+
+    public function logout(string $refreshTokenString): void
+    {
+        $refreshToken = $this->refreshTokenManager->get($refreshTokenString);
+
+        if ($refreshToken) {
+            $this->refreshTokenManager->delete($refreshToken);
+        }
+    }
+
+    public function revokeAllUserTokens(string $username): void
+    {
+        $tokens = $this->refreshTokenManager->getLastFromUsername($username);
+    }
+
+}
