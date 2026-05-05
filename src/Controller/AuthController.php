5032b83a02fecbe34098a2308a9c029a4849340d
ionleon <ionleon@cuatroochenta.com>
2026-05-05 16:31:38 +0200

    - Adapted logout method to manage exceptions.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\AuthManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,24 +13,30 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AuthController extends AbstractController
 {
     public function __construct(
-        private readonly AuthManager $authManager,
+        private readonly AuthManager $authManager
     )
     {}
 
-    #[Route('/logout', name: 'app_logout')]
+    #[Route('/logout', name: 'app_logout', methods: ['POST'])]
     public function logout(Request $request): Response
     {
-        $data = json_decode($request->getContent(), true);
-        $refreshToken = $data['refresh_token'] ?? null;
+        try {
+            $data = json_decode($request->getContent(), true);
+            $refreshToken = $data['refresh_token'] ?? null;
 
-        if (!$refreshToken) {
-            return $this->json(['error' => 'Refresh token required'], 400);
+            if (!$refreshToken) {
+                return $this->json(['error' => 'Refresh token required'], 400);
+            }
+
+            $this->authManager->logout($refreshToken);
+
+            return $this->json(['message' => 'Session closed successfully'], 200);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'An error ocurred during logout',
+                'details'=> $e->getMessage()
+            ], 500);
         }
-
-        $this->authManager->logout($refreshToken);
-
-        return $this->json(['message' => 'Session closed successfully'], 200);
-
 
     }
 }
