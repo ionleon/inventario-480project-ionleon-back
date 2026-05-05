def7f1bdb1c062d018f0673f4fbc51a972937e37
ionleon <ionleon@cuatroochenta.com>
2026-05-05 12:42:19 +0200

    - Created logout route.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Service\AuthManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('', name: 'authentication_logic')]
 final class AuthController extends AbstractController
 {
-    #[Route('/auth', name: 'Authentication logic')]
-    public function index(): Response
+    public function __construct(
+        private readonly AuthManager $authManager,
+    )
+    {}
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(Request $request): Response
     {
-        return $this->render('auth/index.html.twig', [
-            'controller_name' => 'AuthController',
-        ]);
+        $data = json_decode($request->getContent(), true);
+        $refreshToken = $data['refresh_token'] ?? null;
+
+        if (!$refreshToken) {
+            return $this->json(['error' => 'Refresh token required'], 400);
+        }
+
+        $this->authManager->logout($refreshToken);
+
+        return $this->json(['message' => 'Session closed successfully'], 200);
+
+
     }
 }
