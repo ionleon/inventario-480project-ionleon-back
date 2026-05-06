757138ef8b29fa85ad70e98546b9fcfd728fbd3c
ionleon <ionleon@cuatroochenta.com>
2026-05-06 11:49:17 +0200

    - Refactored AuthManager.php.
    - Token decoding extracted to getPayloadFromCurrentRequest method.
    
    - revokeAllUserTokens created, future expansion.
    
    - Removed unnecesary use.

--- src/Service/AuthManager.php
+++ src/Service/AuthManager.php
@@ -2,22 +2,32 @@
 
 namespace App\Service;
 
-use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+
 
 class AuthManager
 {
     public function __construct(
         private readonly RefreshTokenManagerInterface $refreshTokenManager,
         private readonly TokenStorageInterface $tokenStorage,
-        private readonly TagAwareCacheInterface $blacklistCache,
+        private readonly CacheInterface $blacklistCache,
         private readonly JWTTokenManagerInterface $jwtManager,
-        private readonly EntityManagerInterface $em
+        private readonly RequestStack $requestStack,
     ) {}
 
+    /**
+     * @throws JWTDecodeFailureException
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
     public function logout(string $refreshTokenString): void
     {
         $refreshToken = $this->refreshTokenManager->get($refreshTokenString);
@@ -25,11 +35,9 @@ class AuthManager
             $this->refreshTokenManager->delete($refreshToken);
         }
 
-        $token = $this->tokenStorage->getToken();
+        $payload = $this->getPayloadFromCurrentRequest();
 
-        if ($token) {
-            $payload = $this->jwtManager->decode($token);
-            if (isset($payload['jti'], $payload['exp'])) {
+            if ($payload && isset($payload['jti'], $payload['exp'])) {
                 $jti = $payload['jti'];
                 $expiration = $payload['exp'];
                 $ttl = $expiration - time();
@@ -41,9 +49,36 @@ class AuthManager
                     $this->blacklistCache->save($cacheItem);
                 }
             }
+    }
 
+    /**
+     * @throws JWTDecodeFailureException
+     * @throws Exception
+     */
+    private function getPayloadFromCurrentRequest(): ?array
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if ($token) {
+            return $this->jwtManager->decode($token);
         }
 
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request) {
+            return null;
+        }
+
+        $authHeader = $request->headers->get('Authorization');
+        if ($authHeader && str_starts_with($authHeader,  'Bearer ')) {
+            $jwt = substr($authHeader, 7);
+            try {
+                return $this->jwtManager->parse($jwt);
+            } catch (Exception $e) {
+                throw new Exception('Parsing token error: ' . $e->getMessage());
+            }
+        }
+
+        return null;
     }
 
     public function revokeAllUserTokens(string $username): void
