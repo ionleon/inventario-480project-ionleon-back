caf54a5ddf073e07d142aed5e409f47fb5991a06
ionleon <ionleon@cuatroochenta.com>
2026-05-08 11:49:40 +0200

    -  Created LexikPayloadExtractor.php, implements TokenPayloadExtractorInterface.

---
+++ src/Auth/Infrastructure/Service/LexikPayloadExtractor.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Auth\Infrastructure\Service;
+
+use App\Auth\Domain\TokenPayloadExtractorInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class LexikPayloadExtractor implements TokenPayloadExtractorInterface
+{
+
+    public function __construct(
+        private TokenStorageInterface $tokenStorage,
+        private JWTTokenManagerInterface $jwtManager,
+        private RequestStack $requestStack,
+    )
+    {}
+
+    /**
+     * @inheritDoc
+     */
+    public function extractFromCurrentRequest(): ?array
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if ($token && method_exists($token, 'getPayload')) {
+            $payload = $token->getPayload();
+            return $this->formatPayload($payload);
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request) return null;
+
+        $authHeader = $this->requestStack->getCurrentRequest()->headers->get('Authorization');
+        if ($authHeader && str_starts_with($authHeader, 'Bearer ')) {
+            $jwt = substr($authHeader, 7);
+            try {
+                return $this->formatPayload($this->jwtManager->parse($jwt));
+            } catch (\Exception) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    private function formatPayload(array $payload): ?array
+    {
+        if (!$payload || !isset($payload['jti'], $payload['exp'])) {
+            return null;
+        }
+
+        $exp = (int) $payload['exp'];
+        $currentTime = time();
+        $ttl = $exp - $currentTime;
+
+        return [
+            'jti' => (string) $payload['jti'],
+            'exp' => $exp,
+            'ttl' => $ttl,
+        ];
+    }
+}
