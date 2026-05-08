d86fee0b67a0463fb081c07ff5996eff02a3dead
ionleon <ionleon@cuatroochenta.com>
2026-05-08 11:50:14 +0200

    -  Created implementation of RefreshTokenRepositoryInterface.

---
+++ src/Auth/Infrastructure/Persistence/DoctrineRefreshTokenRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Auth\Infrastructure\Persistence;
+
+use App\Auth\Domain\Entity\RefreshToken;
+use App\Auth\Domain\Repository\RefreshTokenRepositoryInterface;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<RefreshToken>
+ */
+class DoctrineRefreshTokenRepository extends ServiceEntityRepository implements RefreshTokenRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, RefreshToken::class);
+    }
+
+    public function revokeAllForUser(string $username): void
+    {
+        $this->createQueryBuilder('r')
+            ->delete()
+            ->where('r.username = :username')
+            ->setParameter('username', $username)
+            ->getQuery()
+            ->execute();
+    }
+
+    public function delete(string $tokenString): void
+    {
+        $token = $this->findOneBy(['refreshToken' => $tokenString]);
+        if ($token) {
+            $this->getEntityManager()->remove($token);
+            $this->getEntityManager()->flush();
+        }
+    }
+}
