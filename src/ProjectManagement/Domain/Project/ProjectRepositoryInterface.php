6880f2d56336103d86ef05d0a0e2c146d681f2a7
ionleon <ionleon@cuatroochenta.com>
2026-05-08 15:18:39 +0200

    -  Added findByFiltersPaginated.

--- src/ProjectManagement/Domain/Project/ProjectRepositoryInterface.php
+++ src/ProjectManagement/Domain/Project/ProjectRepositoryInterface.php
@@ -4,6 +4,7 @@ namespace App\ProjectManagement\Domain\Project;
 
 use App\Entity\AppUser;
 use App\Entity\Client;
+use App\Shared\Domain\Pagination\PaginatedResult;
 use Doctrine\ORM\QueryBuilder;
 
 interface ProjectRepositoryInterface
@@ -12,5 +13,11 @@ interface ProjectRepositoryInterface
     public function findByFilters(ProjectFilters $filters) : array;
     public function findByClient(int $clientId): array;
     public function findByUser(int $userId): array;
+
+    public function findByFiltersPaginated(
+        ProjectFilters $filters,
+        int $page,
+        int $limit
+    ): PaginatedResult;
 
 }
