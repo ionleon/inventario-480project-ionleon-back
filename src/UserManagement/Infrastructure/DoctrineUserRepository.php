b574b4393f4719a615e7e0ae0c407bd511a3ba76
ionleon <ionleon@cuatroochenta.com>
2026-05-12 16:22:03 +0200

    -  DoctrineUserRepository.php created.

--- src/UserManagement/Infrastructure/DoctrineUserRepository.php
+++ src/UserManagement/Infrastructure/DoctrineUserRepository.php
@@ -138,29 +138,6 @@ class DoctrineUserRepository extends ServiceEntityRepository implements AppUserR
     }
 
 
-    //    /**
-    //     * @return AppUser[] Returns an array of AppUser objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?AppUser
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 
 }
