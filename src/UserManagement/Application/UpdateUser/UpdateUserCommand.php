e6e2cea490304990783a045c483a8d977239496b
ionleon <ionleon@cuatroochenta.com>
2026-05-13 16:24:45 +0200

    - Added constructor attributes.

--- src/UserManagement/Application/UpdateUser/UpdateUserCommand.php
+++ src/UserManagement/Application/UpdateUser/UpdateUserCommand.php
@@ -2,7 +2,17 @@
 
 namespace App\UserManagement\Application\UpdateUser;
 
+use App\Shared\Domain\Enum\SystemRole;
+
 class UpdateUserCommand
 {
+    public function __construct(
+        public string       $userId,
+        public ?string      $name       = null,
+        public ?string      $surname    = null,
+        public ?string      $email      = null,
+        public ?SystemRole  $role       = null,
+        public ?bool        $isActive   = null,
+    ) {}
 
 }
