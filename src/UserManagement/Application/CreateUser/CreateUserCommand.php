08531e35b8203d515ee5d1d7349d52ce0e3ad47a
ionleon <ionleon@cuatroochenta.com>
2026-05-13 16:03:02 +0200

    - Added missing attributes.

--- src/UserManagement/Application/CreateUser/CreateUserCommand.php
+++ src/UserManagement/Application/CreateUser/CreateUserCommand.php
@@ -2,14 +2,17 @@
 
 namespace App\UserManagement\Application\CreateUser;
 
+use App\Shared\Domain\Enum\SystemRole;
+
 final readonly class CreateUserCommand
 {
     public function __construct(
         public string $id,
+        public string $email,
+        public string $password,
         public string $name,
         public string $surname,
-        public string $role,
-        public string $password,
+        public SystemRole $role,
     ) {}
 
 }
