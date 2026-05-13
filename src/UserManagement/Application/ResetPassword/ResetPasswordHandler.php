751bd7fa91cbc31c83b66a860f66614e69889c8e
ionleon <ionleon@cuatroochenta.com>
2026-05-13 19:08:09 +0200

    - Implemented logic.

--- src/UserManagement/Application/ResetPassword/ResetPasswordHandler.php
+++ src/UserManagement/Application/ResetPassword/ResetPasswordHandler.php
@@ -2,7 +2,35 @@
 
 namespace App\UserManagement\Application\ResetPassword;
 
+use App\Auth\Application\AuthService;
+use App\UserManagement\Domain\AppUserRepositoryInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class ResetPasswordHandler
 {
+    public function __construct(
+        private readonly AppUserRepositoryInterface  $userRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly AuthService                $authService,
+    ) {}
 
+    public function handle(ResetPasswordCommand $command): void
+    {
+        $user = $this->userRepository->findById($command->userId);
+
+        if (!$user) {
+            throw new \DomainException('User not found');
+        }
+
+        if (strlen($command->newPassword) < 8) {
+            throw new \InvalidArgumentException('New password must be at least 8 characters long.');
+        }
+
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $command->newPassword)
+        );
+
+        $this->userRepository->save($user);
+        $this->authService->forceLogout($user);
+    }
 }
