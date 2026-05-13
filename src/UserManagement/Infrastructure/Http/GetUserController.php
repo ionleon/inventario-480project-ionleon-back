ebfe59efd4b7309129750c5fc7f5953e277c97b3
ionleon <ionleon@cuatroochenta.com>
2026-05-13 14:48:19 +0200

    - Created GetUserController.php.

---
+++ src/UserManagement/Infrastructure/Http/GetUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\UserManagement\Infrastructure\Http;
+
+use App\UserManagement\Application\GetUser\GetUserHandler;
+use App\UserManagement\Application\GetUser\GetUserQuery;
+
+use OpenApi\Attributes as OA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[OA\Tag(name: 'User Management')]
+#[Route('/users/{id}', name: 'app_user_show', methods: ['GET'])]
+class GetUserController extends AbstractController
+{
+    public function __construct(
+      private readonly GetUserHandler $handler
+    ) {}
+
+    public function __invoke(string $id): JsonResponse
+    {
+        try{
+            $user = $this->handler->handle(new GetUserQuery($id));
+            return $this->json($user, 200, [], ['groups' => 'user:read']);
+        } catch (\DomainException $exception) {
+            return $this->json(['error' => $exception->getMessage()], 404, [], ['groups' => 'user:read']);
+        }
+    }
+
+
+}
