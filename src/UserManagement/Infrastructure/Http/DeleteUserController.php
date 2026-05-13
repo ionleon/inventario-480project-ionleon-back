46c747e6f48e83e0cd48dd86ff3c84f680205dee
ionleon <ionleon@cuatroochenta.com>
2026-05-13 13:58:31 +0200

    - Created DeleteUserController.php

---
+++ src/UserManagement/Infrastructure/Http/DeleteUserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\UserManagement\Infrastructure\Http;
+
+use App\UserManagement\Application\DeleteUser\DeleteUserHandler;
+use App\UserManagement\Domain\AppUser;
+use OpenApi\Attributes as OA;
+use App\UserManagement\Application\DeleteUser\DeleteUserCommand;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[OA\Tag(name: 'User Management')]
+#[Route('/{id}', name: 'app_user_delete', methods: ['DELETE'])]
+#[IsGranted('ROLE_ADMIN')]
+class DeleteUserController extends AbstractController
+{
+    public function __construct(
+      private readonly DeleteUserHandler $handler
+    ) {}
+
+    public function __invoke(AppUser $user): JsonResponse
+    {
+        try {
+            $this->handler->handle(new DeleteUserCommand($user->getId()));
+            return $this->json(null, 204);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 400);
+        }
+    }
+}
