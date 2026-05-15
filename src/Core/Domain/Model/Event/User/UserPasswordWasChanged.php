0a12483508de204653baa7816fe62d9e5db3e56b
Virginia Aguado <virginia.aguado@cuatroochenta.com>
2026-05-15 09:11:36 +0200

    feat(user): add User aggregate + domain events (UserWasCreated, Updated, Activated, Deactivated, PasswordWasChanged, RoleWasChanged)

---
+++ src/Core/Domain/Model/Event/User/UserPasswordWasChanged.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Domain\Model\Event\User;
+
+use App\Core\Domain\Model\VO\User\UserId;
+use DateTimeImmutable;
+
+final readonly class UserPasswordWasChanged
+{
+    public function __construct(
+        public UserId $id,
+        public DateTimeImmutable $occurredAt,
+    ) {}
+
+    public static function from(\App\Core\Domain\Model\Aggregate\User $user): self
+    {
+        return new self(
+            id: $user->id(),
+            occurredAt: new DateTimeImmutable(),
+        );
+    }
+}
